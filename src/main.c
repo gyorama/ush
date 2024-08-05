e4fe175c3d3f51db09146c83f6dcbc948eaeea61
Gyorama <Moldy@mx>
2024-08-05 10:09:42 +0300

    Comments & some changes

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// Lots of imports
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,6 +10,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// NOTE: Delete later on 
+char **Tracker; // For debugging purposes
+
+// Char-by-char reading
 void tty_nonBlocking(int fd) {
     struct termios tty;
     tcgetattr(fd, &tty);
@@ -16,7 +21,7 @@ void tty_nonBlocking(int fd) {
     tcsetattr(fd, TCSANOW, &tty);
 }
 
-
+// Reset terminal mode to not fuck everything up on other shells
 void tty_reset(int fd) {
     struct termios tty;
     tcgetattr(fd, &tty);
@@ -24,36 +29,54 @@ void tty_reset(int fd) {
     tcsetattr(fd, TCSANOW, &tty);
 }
 
+// Signal exit
 void end() {
+    uint16_t i = 0;
+    while (Tracker[i] != NULL) {
+        printf("%s ", Tracker[i]);
+        ++i;
+    }    
     tty_reset(STDIN_FILENO);
     exit(0);
 }
 
-int main(int argc, char *args[]) {
+int main(int argc, char *argv[]) {
+    // Signal setup
     signal(SIGINT, end);
     signal(SIGQUIT, end);
+
+    // User setup
     char *name = getenv("USER");
     char *host = getenv("HOSTNAME");
-    char dirBuf[2048];
+    char dirBuf[4096];
 
+    // Token pointer & (Will need this one later)
     char *token;
 
+    // Exit if error on getting the directory
     if (getcwd(dirBuf, 2048) == NULL) {
         puts("Failed to get current directory.\n");
         perror("Error");
         return 1;
     }
 
+    // Replace nothing with "?"
     if (name == NULL) {
         name = "?";
     } else if (host == NULL) {
         host = "?";
     }
 
+    // Set char-by-char reading
     tty_nonBlocking(STDIN_FILENO);
 
+    // Buffers and indexes
     char cmdBuf[4096];
+    char *args[256];
+    Tracker = args;
     uint16_t currIndex = 0;
+    uint16_t argCount = 0;
+
 
     char ch;
 
@@ -63,8 +86,9 @@ int main(int argc, char *args[]) {
             ch = getchar();
             if (ch == '\x7f' || ch == '\b') {
                 printf("\033[D \033[D");
+                cmdBuf[currIndex] = '\0';
                 --currIndex;
-            } else if (ch == '\033') { // Check for escape sequence (beginning of arrow key codes)
+            } else if (ch == '\033') {
                 getchar(); // Skip the '['
                 ch = getchar();
                 if (ch != 'A' && ch != 'B') {
@@ -76,15 +100,18 @@ int main(int argc, char *args[]) {
             ++currIndex;
         }
         ch = '\0';
+
         token = strtok(cmdBuf, " |$;");
 
         while (token) {
-            printf("%s ", token);
+            args[argCount] = token;
+            printf("%s ", args[argCount]);
             token = strtok(NULL, " |$;");
+            ++argCount;
         }
         token = &cmdBuf[0];
+        argCount = 0;
     }
-
 
     return 0;
 }
